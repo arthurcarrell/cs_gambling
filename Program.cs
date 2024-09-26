3dd9bb2b4357aeb9c128dba34458955fd86b11b7
Arthur Carrell <99599003+arthurcarrell@users.noreply.github.com>
2024-09-26 12:39:49 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Template code for console apps
+using System;
+class Program
+{
+    static void Main()
+    {
+        // Here is your empty program!
+        Console.Clear();
+        Console.WriteLine("Hello world!");
+    }
+}
